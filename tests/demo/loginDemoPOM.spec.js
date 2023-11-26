edd8df0d558d089a7f38c8a2a47e885823485f29
AbelOlveraMartinez369 <AbelOlvera74@gmail.com>
2023-11-26 03:08:48 -0600

    1st commit

--- tests/demo/loginDemoPOM.spec.js
+++
@@ -1,9 +0,0 @@
-import {test, expect} from '@playwright/test'
-import { loginPage } from '../../pages/loginPOM'
-
-test('test', async ({page})=>{
-    const Login=new loginPage(page)
-
-    await Login.goToLoginPage()
-    await Login.login('student','Password123')
-})
--- tests/demo/regressionTesting.spec.js
+++ tests/demo/regressionTesting.spec.js
@@ -15,9 +15,14 @@ test.describe('Regression Testing Suite', () => {
 
     test.beforeEach(async () => {
         console.log('Inside Before Each');
+        /*
         await accoreeAdminLogin.clearCookies();
+        await accoreeAdminLogin.clearBrowserCache();
+
         await accoreeAdminLogin.clearLocalStorage();
+
         await accoreeAdminLogin.clearSessionStorage();
+
         await accoreeAdminLogin.setViewportSize({ width: 1920, height: 1080 });
         await accoreeAdminLogin.setPageLoadTimeout(60000);
         await accoreeAdminLogin.setNetworkIdleTimeout(60000);
@@ -27,6 +32,7 @@ test.describe('Regression Testing Suite', () => {
         await accoreeAdminLogin.setGeolocation({ latitude: 37.7749, longitude: -122.4194 });
         await accoreeAdminLogin.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/000000000 Safari/537.36');
         await accoreeAdminLogin.setDeviceScaleFactor(1);
+        */
         await accoreeAdminLogin.startVideoRecording();
         await accoreeAdminLogin.startTracingAndScreenshots(); // Start tracing and screenshots
 
@@ -34,14 +40,14 @@ test.describe('Regression Testing Suite', () => {
 
     test('[JIRA] (SWF-547) Admin Login: To verify that the login button is disabled when no data is entered', async () => {
         try {
-            console.log('Inside Try of Admin Login: To verify that the login button is disabled when no data is entered'); Test;
+            console.log('Inside Try of Admin Login: To verify that the login button is disabled when no data is entered');
             await accoreeAdminLogin.goToAccoreeAdminLoginPage();
             await accoreeAdminLogin.enterEmail('abelm@tlcengine.com');
             await accoreeAdminLogin.enterPassword('Password123@');
             await accoreeAdminLogin.clickOnRememberMeCheckbox();
             await accoreeAdminLogin.clickOnLoginButton();
         } catch (error) {
-            console.log('Inside Catch of Admin Login: To verify that the login button is disabled when no data is entered'); Test;
+            console.log('Inside Catch of Admin Login: To verify that the login button is disabled when no data is entered');
             console.error('Error:', error);
         }
     });
